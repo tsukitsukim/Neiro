99e8774dc995a4edf744aff6a48eb66b14328cac
Plizik <channelofplizik@gmail.com>
2023-10-05 23:10:18 +0300

    .

--- NeiroWeb/NeiroWeb/settings.py
+++ NeiroWeb/NeiroWeb/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
