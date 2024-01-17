2dbf01fc0b41a30360b00969848cf3b2dc8b9039
= <=>
2024-01-17 07:22:27 +0300

    .

--- ArtificialIntelligence/NeiroLM-1/__init__.py
+++ ArtificialIntelligence/NeiroLM-1/__init__.py
@@ -1,7 +1,3 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow import keras
-import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-print("TensorFlow version:", tf.__version__)
--- ArtificialIntelligence/NeiroLM-1/database/script.sh
+++
@@ -1,9 +0,0 @@
-!wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
-!unzip uncased_L-12_H-768_A-12.zip
-!wget https://nlp.stanford.edu/projects/snli/snli_1.0.zip
-!unzip snli_1.0.zip
-!wget https://www.nyu.edu/projects/bowman/multinli/multinli_1.0.zip
-!unzip multinli_1.0.zip
-
-!git clone https://github.com/brmson/dataset-sts
-!git clone https://github.com/gaphex/bert_experimental
